a747cff04e29dd13027c963084a0310d5bbbf70d
YousifWahed005 <yousifayman004@gmail.com>
2026-07-18 12:55:26 +0300

    new fixes

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,7 @@ const nextConfig = {
   experimental: {
     serverActions: { bodySizeLimit: "12mb" },
   },
+  turbopack: {},
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = { ...config.resolve.fallback, "iconv-lite": false, canvas: false };
